f208780f5a67f2f0a90d702bca0e0056f4fca010
mnaynes <mcnaynes@up.edu.ph>
2024-11-11 16:25:29 +0800

    vercel deployment testing

--- send_personalized_emails/api/index.py
+++ send_personalized_emails/api/index.py
@@ -1,13 +1,32 @@
-from http.server import BaseHTTPRequestHandler
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-#!/usr/bin/python
-from src.send_personalized_emails import send_personalized_emails
+app = FastAPI()
 
-class handler(BaseHTTPRequestHandler):
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+# from http.server import BaseHTTPRequestHandler
+
+# #!/usr/bin/python
+# from src.send_personalized_emails import send_personalized_emails
+
+# class handler(BaseHTTPRequestHandler):
  
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        send_personalized_emails("../data/websites.txt", "../data/AIA Sales Hacker.docx")
-        return
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','text/plain')
+#         self.end_headers()
+#         send_personalized_emails("../data/websites.txt", "../data/AIA Sales Hacker.docx")
+#         return
